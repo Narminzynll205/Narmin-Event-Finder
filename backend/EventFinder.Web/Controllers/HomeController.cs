e74d8ac22373b550a7d7bb61791c486c937b826e
Event Finder Dev <dev@eventfinder.local>
2026-08-02 17:33:03 +0400

    Add Home/landing page

--- backend/EventFinder.Web/Controllers/HomeController.cs
+++ backend/EventFinder.Web/Controllers/HomeController.cs
@@ -1,21 +1,27 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using EventFinder.Web.Models;
+using EventFinder.Web.DTOs.Events;
+using EventFinder.Web.Services;
 
 namespace EventFinder.Web.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly IEventService _eventService;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, IEventService eventService)
     {
         _logger = logger;
+        _eventService = eventService;
     }
 
-    public IActionResult Index()
+    public async Task<IActionResult> Index()
     {
-        return View();
+        var upcoming = await _eventService.GetAllAsync(new EventFilterDto());
+        var recentEvents = upcoming.Take(8).ToList();
+        return View(recentEvents);
     }
 
     public IActionResult Privacy()
